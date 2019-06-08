16f6ceb8962c5cbbed971750704aece9ee80e486
Adina Wagner <adina.wagner@t-online.de>
2019-06-08 11:01:54 +0200

    add initial setup.py, have main function in entrypoint called segstats2nidm

---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+from os.path import join as opj
+from os.path import dirname
+
+
+def get_version():
+    """Load version only
+    """
+    with open(opj(dirname(__file__), 'segstats_jsonld', '__init__.py')) as f:
+        version_lines = list(filter(lambda x: x.startswith('__version__'), f))
+    assert (len(version_lines) == 1)
+    return version_lines[0].split('=')[1].strip(" '\"\t\n")
+
+# extension version
+version = get_version()
+
+
+README = opj(dirname(__file__), 'README.md')
+try:
+    import pypandoc
+    long_description = pypandoc.convert(README, 'rst')
+except (ImportError, OSError) as exc:
+    print(
+        "WARNING: pypandoc failed to import or threw an error while converting"
+        " README.md to RST: %r  .md version will be used as is" %exc
+    )
+    long_description = open(README).read()
+
+# Metadata
+setup(
+    name='segstats_jsonld',
+    version=version,
+    description='Freesurfer metadata extraction & conversion to jsonld',
+    long_description=long_description,
+    author='David Keator',
+    author_email='TODO',
+    url='https://github.com/dbkeator/segstats_jsonld',
+    packages=[pkg for pkg in find_packages('.') if pkg.startswith('segstats_jsonld')],
+    install_requires=[
+        'numpy',
+        'pandas'
+    ], # Add requirements as necessary
+    extras_require={
+        'devel-docs': [
+            # for converting README.md -> .rst for long description
+            'pypandoc',
+        ]},
+    entry_points={
+        'console_scripts': [
+            'segstats2nidm=segstats_jsonld.fs_to_nidm:main' # this is where the console entry points are defined
+            ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], # Change if necessary
+)
+
