b9ce445e82ac8d58bb9fa62341b7686672aa4e52
Adina Wagner <adina.wagner@t-online.de>
2019-06-08 10:33:47 +0200

    Restructuring to prepare packaging, add __init__.py file

---
+++ segstats_jsonld/__init__.py
@@ -0,0 +1,13 @@
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See LICENSE file distributed along with the segstats_jsonld package for the
+#   license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+__version__ = 0.0.1
+
+# do imports of all of the functions that should be available here
+from .segstats_jsonld import #TODO
