aa11471274fad05d5215aa4fa34a928278499d18
Adina Wagner <adina.wagner@t-online.de>
2019-06-24 09:49:54 +0200

    start a file for testing with pytest

---
+++ segstats_jsonld/tests/test_fs_to_nidm.py
@@ -0,0 +1,24 @@
+import pytest
+from .. import fs_to_nidm as s
+
+
+def test_remap2json():
+    """
+
+    """
+    xlsx_file = '../mapping_data/ReproNimCDEs.xlsx'
+    aparc_example = 'testdata/rh.aparc.stats'
+    asag_example = 'aseg.stats'
+
+    # check whether non-scraping works
+    for examplefile in [aparc_example, asag_example]:
+        header, tableinfo, measures, biggie = s.remap2json(xlsx_file,
+                                                             examplefile,
+                                                             noscrape=True
+                                                             )
+        # assert that definitions are not assigned
+        for d in ['Anatomy', 'Measures']:
+            for key, value in biggie[d].items():
+                for k, v in biggie[d][key].items():
+                    if k == 'definition':
+                        assert v == ""
