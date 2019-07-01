d3e1c186ad7f939d571b36dd7103aa3906f26f33
Adina Wagner <adina.wagner@t-online.de>
2019-07-01 16:06:41 +0200

    adjust existing tests to changes in base-mapper, make data available from other places than

--- segstats_jsonld/tests/test_fs_to_nidm.py
+++ segstats_jsonld/tests/test_fs_to_nidm.py
@@ -1,36 +1,50 @@
 import pytest
+import json
 import numpy as np
+from os.path import join, exists
+from .. import mapping_data
+from . import testdata
 from .. import fs_to_nidm as s
 
 
+datapath = mapping_data.__path__._path[0] + '/'
+testdatap = testdata.__path__._path[0] + '/'
+
 def test_remap2json():
     """
-
+    Test some basic functionality of remap2json
     """
-    xlsx_file = '../mapping_data/ReproNimCDEs.xlsx'
-    aparc_example = 'testdata/rh.aparc.stats'
-    asag_example = 'testdata/aseg.stats'
 
-    # check whether non-scraping works
+    xlsx_file = join(datapath, 'ReproNimCDEs.xlsx')
+    aparc_example = join(testdatap, 'rh.aparc.stats')
+    asag_example = join(testdatap, 'aseg.stats')
+
+    # smoke test whether non-scraping works
     for examplefile in [aparc_example, asag_example]:
         header, tableinfo, measures, biggie = s.remap2json(xlsx_file,
                                                              examplefile,
                                                              noscrape=True
                                                              )
-        # assert that definitions are not assigned
-        for d in ['Anatomy', 'Measures']:
-            for key, value in biggie[d].items():
-                for k, v in biggie[d][key].items():
-                    if k == 'definition':
-                        assert v == ""
+        # assert that definitions are not assigned if there is no base-mapper
+        if exists(join(datapath, 'jsonmap.json')):
+            # TODO
+            continue
+        else:
+            # if there is no remapping file present yet:
+            for d in ['Anatomy', 'Measures']:
+                for key, value in biggie[d].items():
+                    for k, v in biggie[d][key].items():
+                        if k == 'definition':
+                            assert v == ""
 
-    # check whether scraping works
+    # smoke test whether scraping works
     if s.test_connection():
         for examplefile in [aparc_example, asag_example]:
             header, tableinfo, measures, biggie = s.remap2json(xlsx_file,
                                                                examplefile,
-                                                               noscrape=False
-                                                                   )
+                                                               noscrape=False,
+                                                               force_update=True
+                                                                )
             for d in ['Anatomy', 'Measures']:
                 for key, value in biggie[d].items():
                     for k, v in biggie[d][key].items():
@@ -45,3 +59,10 @@ def test_remap2json():
 
             elif examplefile == asag_example:
                 assert biggie['Measures']['NVoxels']['measureOf'] == 'http://uri.interlex.org/base/ilx_0105536#'
+
+    # check whether we save valid json
+    with open(join(testdatap, 'tmpjson.json'), 'w') as f:
+        json.dump(biggie, f, indent=4)
+    # parsing this will fail if json is not valid
+    with open(join(testdatap, 'tmpjson.json'), 'r') as j:
+        json.load(j)
